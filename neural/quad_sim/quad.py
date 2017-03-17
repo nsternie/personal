ca62ab63046ed2e0a3b8ccd4247eddd31c31beea
nsternie <nsternie@umich.edu>
2017-03-16 23:02:15 -0700

    Basic structure of quad module complete

--- neural/quad_sim/quad.py
+++ neural/quad_sim/quad.py
@@ -1,3 +1,10 @@
+# Nick Sterenberg - nsternie@umich.edu
+#
+## Quadcopter simulation module ##
+# This library is created with the intend of simulating
+# the dynamics of quadcopter flight.
+
+
 # Motor numbering scheme is
 #
 #   A
@@ -13,6 +20,8 @@
 #  Back (-X)
 #   |
 #   V
+#
+# 0 spins clockwise! (Im too lazy to make this an option in the config, live with it)
 
 import math
 
@@ -21,12 +30,13 @@ class Quadcopter(object):
 	def __init__(self, config_file):
 		
 		self.throttle = [0.0,0.0,0.0,0.0] 	# Throttle Percent of all motors
-		self.thrust = [0.0,0.0,0.0]			# Thrust from each of the motors
+		self.thrust = [0.0,0.0,0.0,0.0]		# Thrust from each of the motors
+		self.motor_torques = [0.0,0.0,0.0,0.0]	# Torqu pushback from each motor
 		self.rates = [0.0,0.0,0.0] 			# Vehicle rates in radians per second, Rx, Ry, Rz
-		self.angle = [0.0,0.0,0.0]			# Angles of each axis from zero
-		self.moments = [0.0,0.0,0.0] 		# Torqe on the three axes of the vehice
-		# Time in the sim of the vehicle
-		self.time = 0
+		self.angles = [0.0,0.0,0.0]			# Angles of each axis from zero
+		self.alpha = [0.0,0.0,0.0]			# Angular acceleration of the vehicle
+		self.torques = [0.0,0.0,0.0] 		# Torqe on the three axes of the vehice
+		self.time = 0						# Time in the sim of the vehicle
 		
 		# Open the config file
 		self.config = open(config_file, 'r')
@@ -42,9 +52,14 @@ class Quadcopter(object):
 			except:
 				self.variables[var] = str(val)
 
-		self.thrust_curve = open(variables['thrust.curve'], 'r')
-		self.torque_curve = open(variables['torgue.curve'], 'r')
-
+		# Strings that are the equation for thrust in N as a function of throttle T
+		self.thrust_curve = self.variables['thrust_curve']
+		self.torque_curve = self.variables['torque_curve']
+		self.moments = [0.0,0.0,0.0]
+		self.moments[0] = self.variables['I_X']
+		self.moments[1] = self.variables['I_Y']
+		self.moments[2] = self.variables['I_Z']
+		self.timestep = self.variables['timestep']
 
 	def print_variables(self):
 		print self.variables
@@ -56,20 +71,38 @@ class Quadcopter(object):
 		self.throttle = throttles
 
 	def step(self):
-		self.calculate_thrusts()
-		self.calculate_moments()
-		self.calculate_rates()
-		self.calculate_angles()
-		self.time = self.time + self.variables['timestep']
+		self.update_kinematics()
+		self.time = self.time + self.timestep
 
-	def calculate_thrusts(self):
+	def update_kinematics(self):
+		# Thrust calc #####################################
+		for n in range(0,4):
+			T = self.throttle[n]
+			self.thrust[n] = eval(self.thrust_curve)
+		
+		# Torque calc #####################################
+		# Length of the [virtual] moment arm of each of these forces, in cardinal directions, not arm directions
+		moment_arm = math.sqrt(2) * self.variables['arm_length']
+		# Calculate the forces on the quad in each axis (Nm)
+		# X
+		self.torques[0] = (self.thrust[0] + self.thrust[3]) / moment_arm
+		# Y 
+		self.torques[1] = (self.thrust[0] + self.thrust[1]) / moment_arm
+		# Z
+		# First we have to calculate the torque on the vehicle as a result of each of the motors
+		# then, we just sum them to get the torque on the vehicle about the Z axis
+		for motor in range(0,4):
+			T = self.throttle[n]
+			self.motor_torques[n] = eval(self.torque_curve)
+		# Invert the torques from motors 1 and 3 since they spin counter clockwise
+		self.motor_torques[1] *= 1
+		self.motor_torques[3] *= 1
 
+		self.torques[2] = sum(self.motor_torques)
+		
 
-	def calculate_moments(self):
-		forces = [0.0,0.0,0.0,0.0]	# Y+, X+, Y-, X-
-		forces[0] = 
-
-	def calculate_rates(self):
-
-	def calculate_angles(self):
-
+		# Angular calculations ############################
+		for n in range(0,3):
+			self.alpha[n] = self.torques[n]/self.moments[n]
+			self.rates[n] = self.rates[n] + (self.alpha[n]/self.timestep)
+			self.angles[n] = self.angles[n] + (self.rates[n]/self.timestep)
--- neural/quad_sim/quad_sim.py
+++ neural/quad_sim/quad_sim.py
@@ -1,4 +1,13 @@
 import quad
 
 q = quad.Quadcopter('quad.config')
-q.print_variables()
+
+#q.print_variables()
+
+t = [50,50,50,50]
+
+q.set_throttle(t)
+
+print q.thrust
+q.step()
+print q.thrust
