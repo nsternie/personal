d62f35595da10bcbbfa33c2ba4708a4cd073a35e
nsternie <nsternie@umich.edu>
2017-03-18 12:35:18 -0700

    2d-nav now iterates several samples per net iteration. Added single layer config file format. Added imu folder with graphics module

--- neural/2d-nav/2d-nav.py
+++ neural/2d-nav/2d-nav.py
@@ -10,7 +10,8 @@ neurons = []
 ##################################
 # Configuration
 num_neurons = 5			# How many neurons will be used in the neural network
-iterations = 100000
+iterations = 100
+test_cases = 100000
 weight_range = 5
 bias_range = 20
 ##################################
@@ -18,21 +19,20 @@ bias_range = 20
 dx = neuron.Neuron(num_neurons)
 dy = neuron.Neuron(num_neurons)
 lowest_error = 10000
+best_weights = []
+best_biases = []
 
 for n in range(0, num_neurons):
 	neurons.append(neuron.Neuron(4))
 
-for loop in range(0, iterations):
+for loop in range(0, test_cases):
 	# Generate the info used to train it. Look at pointGen.py for more info
-	raw = pg.generate(0,1)
-	inputs = raw[0:4]
-	intermediate = []
+	
 	# Generate random weights and bias for each neuron
 	for n in range(0, num_neurons):
 		neurons[n].weights = [rand.uniform(-weight_range, weight_range),rand.uniform(-weight_range, weight_range),rand.uniform(-weight_range, weight_range),rand.uniform(-weight_range, weight_range)]
 		neurons[n].bias = rand.uniform(0,bias_range)
-		neurons[n].update(inputs)
-		intermediate.append(neurons[n].out)
+		
 	# Generate weights for the output neurons
 	for m in range(0, num_neurons):
 		dx.weights[m] = rand.uniform(-weight_range, weight_range)
@@ -40,19 +40,28 @@ for loop in range(0, iterations):
 
 	dx.bias = rand.uniform(0, bias_range)
 	dy.bias = rand.uniform(0, bias_range)
+	tot_err = 0
+	for foo in range(0, iterations):
+		raw = pg.generate(0,1)
+		inputs = raw[0:4]
+		intermediate = []
+		for n in range(0, num_neurons):
+			neurons[n].update(inputs)
+			intermediate.append(neurons[n].out)
+		dx.update(intermediate)
+		dy.update(intermediate)
 
-	dx.update(intermediate)
-	dy.update(intermediate)
+		x_error = dx.out - raw[4]
+		y_error = dy.out - raw[5]
 
-	x_error = dx.out - raw[4]
-	y_error = dy.out - raw[5]
+		abs_error = ((x_error**2)+(y_error**2))**(0.5)
+		tot_err += abs_error
+	avg_error = tot_err/iterations
+	if avg_error < lowest_error:
+		lowest_error = avg_error
+		print avg_error
 
-	abs_error = ((x_error**2)+(y_error**2))**(0.5)
-	if abs_error < lowest_error:
-		lowest_error = abs_error
-		print abs_error
-
-print 'Lowest error attained was',
+print 'Lowest average error attained was',
 print lowest_error
 
 
